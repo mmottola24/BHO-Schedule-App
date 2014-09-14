495344933f091e206fa065e18f14ce5d245e4926
Michael Mottola <mikemottola@gmail.com>
2014-09-13 22:51:49 -0400

    Add some routes for team schedules.

--- app/controllers/visitors_controller.rb
+++ app/controllers/visitors_controller.rb
@@ -1,6 +1,16 @@
 class VisitorsController < ApplicationController
 
   def index
-    
+
   end
+
+  def team_vendetta
+
+  end
+
+  def team_weekend_warriors
+
+  end
+
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
   root to: 'visitors#index'
+
+  get 'team/vendetta', to: 'visitors#team_vendetta'
+  get 'team/weekend-warriors', to: 'visitors#team_weekend_warriors'
 end
