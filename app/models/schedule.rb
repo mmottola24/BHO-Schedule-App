d863b142d5edd61695e87aa20c970e11dc97624e
Michael Mottola <mikemottola@gmail.com>
2014-09-14 02:06:23 -0400

    Build Schedule Model.

---
+++ app/models/schedule.rb
@@ -0,0 +1,38 @@
+class Schedule
+  include HTTParty
+
+  def get_full_schedule
+
+  end
+
+  def self.get_team_schedule team_name
+    team_name = team_name.downcase.gsub(' ', '_')
+    response = HTTParty.get("http://www.bho.michaelencode.com/team_schedule_#{team_name}.json")
+
+    self.parse_team_schedule response
+  end
+
+  private
+
+  def self.parse_team_schedule response
+    schedule                  = {}
+    schedule[:future_games]   = []
+    schedule[:past_games]     = []
+
+    if response.code != 200
+      false
+    else
+      raw_schedule = JSON.parse(response.body)
+
+      raw_schedule.each do |game|
+        if Time.parse("#{game['Date']} #{game['Start']}").future?
+          schedule[:future_games].push game
+        else
+          schedule[:past_games].push game
+        end
+      end
+    end
+
+    schedule
+  end
+end
