db1b7dafdeb20b268d4f95f09ff12891c4f01b1b
Michael Mottola <mikemottola@gmail.com>
2016-08-29 00:33:17 -0400

    Add last played time and result to the game list

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,15 @@
 module ApplicationHelper
+
+  def get_game_result game, team_name
+    result = ''
+    if game['home_points'] == game['visitor_points']
+      result = "T #{game['home_points']}-#{game['visitor_points']}"
+    elsif game['home_team'] == team_name
+      result = (game['home_points'] > game['visitor_points']) ? 'W' : 'L'
+      result << " #{game['home_points']}-#{game['visitor_points']}"
+    else
+      result = (game['visitor_points'] > game['home_points']) ? 'W' : 'L'
+      result << " #{game['visitor_points']}-#{game['home_points']}"
+    end
+  end
 end
